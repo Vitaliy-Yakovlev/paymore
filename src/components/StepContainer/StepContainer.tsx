66ba305c7888c7d4b445aa920d941f990bec387c
Виталий <74872939+Vitaliy-Yakovlev@users.noreply.github.com>
2025-11-17 17:32:33 +0100

    fixed the question blocks

--- src/components/StepContainer/StepContainer.tsx
+++ src/components/StepContainer/StepContainer.tsx
@@ -7,6 +7,12 @@ export interface StepContainerProps {
 }
 
 const StepContainer: React.FC<StepContainerProps> = ({ children, step }) => {
+  const stepName: Record<number, string> = {
+    1: 'select item',
+    2: 'item price',
+    3: 'item condition',
+    4: '',
+  };
   return (
     <div className={css.stepContainer}>
       <div className='wrapper-step-current'>
@@ -14,7 +20,7 @@ const StepContainer: React.FC<StepContainerProps> = ({ children, step }) => {
           <p className='step-text'>
             STEP {step} / {4}
           </p>
-          <p className='step-text'>Item price</p>
+          <p className='step-text'>{stepName[step]}</p>
         </div>
         <div className='step-current'>
           {Array.from({ length: 4 }, (_, index) => index + 1).map(stepIndex => (
